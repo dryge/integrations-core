40595470147ec66964e80fdd0af6a5cf010a1f6c
Florent Clarret <florent.clarret@datadoghq.com>
2022-07-28 09:00:21 +0200

    Fix the service check

--- impala/datadog_checks/impala/metrics.py
+++ impala/datadog_checks/impala/metrics.py
@@ -1,50 +1,50 @@
 CATALOG_METRIC_MAP = {
-    "impala_jvm_gc_count": "jvm.gc",
+    "impala_jvm_gc_count": "catalog.jvm.gc",
 }
 
 DAEMON_METRIC_MAP = {
-    "impala_jvm_gc_count": "jvm.gc",
+    "impala_jvm_gc_count": "daemon.jvm.gc",
 }
 
 STATESTORE_METRIC_MAP = {
-    "impala_statestore_live_backends": "live_backends",
-    # "impala_statestore_total_key_size_bytes": "total_key_size",
-    # "impala_statestore_total_value_size_bytes": "total_value_size",
-    # "impala_statestore_topic_update_durations": "topic_update_durations",
-    # "impala_statestore_total_topic_size_bytes": "total_topic_size",
-    # "impala_statestore_priority_topic_update_durations": "priority_topic_update_durations",
-    # "impala_statestore_heartbeat_durations": "heartbeat_durations",
+    "impala_statestore_live_backends": "statestore.live_backends",
+    # "impala_statestore_total_key_size_bytes": "statestore.total_key_size",
+    # "impala_statestore_total_value_size_bytes": "statestore.total_value_size",
+    # "impala_statestore_topic_update_durations": "statestore.topic_update_durations",
+    # "impala_statestore_total_topic_size_bytes": "statestore.total_topic_size",
+    # "impala_statestore_priority_topic_update_durations": "statestore.priority_topic_update_durations",
+    # "impala_statestore_heartbeat_durations": "statestore.heartbeat_durations",
     # "impala_rpc_method_statestore_StatestoreService_RegisterSubscriber_call_duration":
-    # "rpc_method_register_subscriber_call_duration",
+    # "statestore.rpc_method_register_subscriber_call_duration",
     # # thrift_server
-    # "impala_thrift_server_StatestoreService_connection_setup_time": "thrift_server.connection_setup_time",
-    # "impala_thrift_server_StatestoreService_svc_thread_wait_time": "thrift_server.svc_thread_wait_time",
+    # "impala_thrift_server_StatestoreService_connection_setup_time": "statestore.thrift_server.connection_setup_time",
+    # "impala_thrift_server_StatestoreService_svc_thread_wait_time": "statestore.thrift_server.svc_thread_wait_time",
     # "impala_thrift_server_StatestoreService_connection_setup_queue_size":
-    # "thrift_server.connection_setup_queue_size",
-    # "impala_thrift_server_StatestoreService_connections_in_use": "thrift_server.connections_in_use",
+    # "statestore.thrift_server.connection_setup_queue_size",
+    # "impala_thrift_server_StatestoreService_connections_in_use": "statestore.thrift_server.connections_in_use",
     # "impala_thrift_server_StatestoreService_timedout_cnxn_requests":
-    # "thrift_server.timedout_cnxn_requests",
-    # "impala_thrift_server_StatestoreService_total_connections": "thrift_server.total_connections",
+    # "statestore.thrift_server.timedout_cnxn_requests",
+    # "impala_thrift_server_StatestoreService_total_connections": "statestore.thrift_server.total_connections",
     # # thread_manager
     # "impala_subscriber_heartbeat_client_cache_clients_in_use":
-    # "subscriber.heartbeat.client_cache_clients.in_use",
+    # "statestore.subscriber.heartbeat.client_cache_clients.in_use",
     # "impala_subscriber_heartbeat_client_cache_total_clients":
-    # "subscriber.heartbeat.client_cache.total_clients",
+    # "statestore.subscriber.heartbeat.client_cache.total_clients",
     # "impala_subscriber_update_state_client_cache_clients_in_use":
-    # "subscriber.update_state_client_cache.clients_in_use",
+    # "statestore.subscriber.update_state_client_cache.clients_in_use",
     # "impala_subscriber_update_state_client_cache_total_clients":
-    # "subscriber.update_state_client_cache.total_clients",
+    # "statestore.subscriber.update_state_client_cache.total_clients",
     # # thread_manager
-    # "impala_thread_manager_total_threads_created": "thread_manager.total_threads_created",
-    # "impala_thread_manager_running_threads": "thread_manager.running_threads",
+    # "impala_thread_manager_total_threads_created": "statestore.thread_manager.total_threads_created",
+    # "impala_thread_manager_running_threads": "statestore.thread_manager.running_threads",
     # # memory
-    # "impala_memory_rss": "memory.rss",
-    # "impala_memory_total_used": "memory.total_used",
-    # "impala_memory_mapped_bytes": "memory.mapped",
+    # "impala_memory_rss": "statestore.memory.rss",
+    # "impala_memory_total_used": "statestore.memory.total_used",
+    # "impala_memory_mapped_bytes": "statestore.memory.mapped",
     # # tcmalloc
-    # "impala_tcmalloc_physical_bytes_reserved": "tcmalloc.physical_bytes_reserved",
-    # "impala_tcmalloc_bytes_in_use": "tcmalloc.bytes.in_use",
-    # "impala_tcmalloc_total_bytes_reserved": "tcmalloc.total_bytes_reserved",
-    # "impala_tcmalloc_pageheap_free_bytes": "tcmalloc.pageheap.free_bytes",
-    # "impala_tcmalloc_pageheap_unmapped_bytes": "tcmalloc.pageheap_unmapped_bytes",
+    # "impala_tcmalloc_physical_bytes_reserved": "statestore.tcmalloc.physical_bytes_reserved",
+    # "impala_tcmalloc_bytes_in_use": "statestore.tcmalloc.bytes.in_use",
+    # "impala_tcmalloc_total_bytes_reserved": "statestore.tcmalloc.total_bytes_reserved",
+    # "impala_tcmalloc_pageheap_free_bytes": "statestore.tcmalloc.pageheap.free_bytes",
+    # "impala_tcmalloc_pageheap_unmapped_bytes": "statestore.tcmalloc.pageheap_unmapped_bytes",
 }
